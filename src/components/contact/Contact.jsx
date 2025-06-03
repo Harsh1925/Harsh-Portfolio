df26293c73d30efba1ec5388833126b5b94f22e5
Harsh1925 <d007modi@gmail.com>
2025-06-03 19:43:53 -0400

    new

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,33 @@
 import "./contact.scss";
 
 const Contact = () => {
-  return <div className="contact">Contact</div>;
+  return (
+    <div className="contact">
+      <div className="textContainer">
+        <h1>Lets work together</h1>
+        <div className="item">
+          <h2>Mail</h2>
+          <span>harshmodi134@gmail.com</span>
+        </div>
+        <div className="item">
+          <h2>Address</h2>
+          <span>Hello street New York </span>
+        </div>
+        <div className="item">
+          <h2>Phone</h2>
+          <span>+1 551 260 8493</span>
+        </div>
+      </div>
+      <div className="formContainer">
+        <form>
+          <input type="text" required placeholder="Name" />
+          <input type="email" required placeholder="Email" />
+          <textarea rows={8} placeholder="Message" />
+          <button>Submit</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -11,28 +11,39 @@ import { useRef } from "react";
 
 const items = [
   {
+    id: 0,
+    title: "HouseHunt (Real-Estate Web App)",
+    img: "https://images.pexels.com/photos/943096/pexels-photo-943096.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    desc: "A full-stack MERN (MongoDB, Express.js, React, Node.js) based real estate web application featuring real-time chat, JWT authentication, property listing management, and secure user sessions. Built for scalability, performance, and modern UX.",
+    demo: "https://github.com/Harsh1925/HouseHunt",
+  },
+  {
     id: 1,
-    title: "React Commerce",
-    img: "https://images.pexels.com/photos/196655/pexels-photo-196655.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    desc: "This here is the decription of the item and I am just writing whatever I want to write and you should know tha t and I want you to know that",
+    title: "Google Drive Clone (Full stack Webapp) ",
+    img: "https://images.pexels.com/photos/218717/pexels-photo-218717.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    desc: "Developed a file management and sharing platform using Next.js, React, and Appwrite, enabling seamless file uploads, organization, and sharing for over 5,000+ users, with advanced features like global search and sorting options to improve accessibility by 35%.",
+    demo: "https://github.com/Harsh1925/Google-Drive-Clone",
   },
   {
     id: 2,
-    title: "Next.JS Commerce",
-    img: "https://images.pexels.com/photos/60504/security-protection-anti-virus-software-60504.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    desc: "This here is the decription of the item and I am just writing whatever I want to write and you should know tha t and I want you to know that",
+    title: "Squaddle (Match Making Web Application)",
+    img: "https://images.pexels.com/photos/196655/pexels-photo-196655.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    desc: "Innovated Squaddle, a MERN-based matchmaking platform merging Tinder-like swiping with LinkedIn's professionalism to increase in user collaboration. Leveraged HTML, CSS, JavaScript, ReactJS, Express.js, MongoDB, and NodeJS to construct Squaddle.",
+    demo: "https://github.com/Harsh1925/Match-Making-Web-Application-Squaddle",
   },
   {
     id: 3,
-    title: "Vanilla.js Commerce",
+    title: "Party-Picasso (Full stack Webapp) ",
     img: "https://images.pexels.com/photos/38568/apple-imac-ipad-workplace-38568.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    desc: "This here is the decription of the item and I am just writing whatever I want to write and you should know tha t and I want you to know that",
+    desc: "Party Picasso is your one-stop platform for exploring and attending a diverse range of exciting events happening around you. Whether you're looking for vibrant music concerts, engaging seminars, cutting-edge tech fairs, or lively community gatherings, Party Picasso has something for everyone.",
+    demo: "https://github.com/Harsh1925/partypicasso",
   },
   {
     id: 4,
-    title: "Music app",
+    title: "Eventizer (Twitter / X Clone App) ",
     img: "https://images.pexels.com/photos/267350/pexels-photo-267350.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    desc: "This here is the decription of the item and I am just writing whatever I want to write and you should know tha t and I want you to know that",
+    desc: "Eventizer, a social media web application leveraging the MERN stack, simplifying event sharing and discovery using React.js and Redux-based user interface. Integrated advanced features that fostered a vibrant community of enthusiasts, enhancing event exploration and sharing experiences.",
+    demo: "https://github.com/Harsh1925/Eventizer",
   },
 ];
 
@@ -55,7 +66,9 @@ const Single = ({ item }) => {
           <motion.div className="textContainer" style={{ y }}>
             <h2>{item.title}</h2>
             <p>{item.desc}</p>
-            <button>See Demo</button>
+            <button>
+              <a href={item.demo}>See Demo</a>
+            </button>
           </motion.div>
         </div>
       </div>
