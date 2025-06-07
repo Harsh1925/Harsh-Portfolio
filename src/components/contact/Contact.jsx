64be2e06ef2e61d73428939e9ed0eb080210205c
Harsh1925 <d007modi@gmail.com>
2025-06-07 19:48:07 -0400

    new

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,7 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import "./contact.scss";
 import { motion, useInView } from "framer-motion";
+import emailjs from "@emailjs/browser";
 
 const variants = {
   initial: {
@@ -19,7 +20,33 @@ const variants = {
 
 const Contact = () => {
   const ref = useRef();
+  const formRef = useRef();
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
   const isInView = useInView(ref, { margin: "-100px" });
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_xjca95t", "template_pdc7l3r", formRef.current, {
+        publicKey: "weAmbei8mexWQCIyO",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+          setSuccess(true);
+          setError(false);
+          formRef.current.reset(); // ← clear the form fields
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+          setError(true);
+          setSuccess(false);
+        }
+      );
+  };
+
   return (
     <motion.div
       ref={ref}
@@ -90,14 +117,18 @@ const Contact = () => {
           </motion.svg>
         </motion.div>
         <motion.form
+          ref={formRef}
+          onSubmit={sendEmail}
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
           transition={{ delay: 4, duration: 1 }}
         >
-          <input type="text" required placeholder="Name" />
-          <input type="email" required placeholder="Email" />
-          <textarea rows={8} placeholder="Message" />
+          <input type="text" required placeholder="Name" name="name" />
+          <input type="email" required placeholder="Email" name="email" />
+          <textarea rows={8} placeholder="Message" name="message" />
           <button>Submit</button>
+          {error && "Error"}
+          {success && "Success"}
         </motion.form>
       </div>
     </motion.div>
